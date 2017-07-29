4b42961604e59d909b9dfe6152f0e6ea7b980440
m0zal <most_wanted@list.ru>
2017-07-29 13:32:32 +0200

    heder_works_2

--- app/override/header_update.rb
+++ app/override/header_update.rb
@@ -1,4 +1,4 @@
 Deface::Override.new (virtual_path => 'spree/shared/_header'),
 :name => 'change header logo',
-:replace => "div#header",
-:text => "Gifticool")
+:replace => "figure#logo",
+:text => "<a href="/"><img src="/assets/logo/logogrey.png" alt="Gifticool"></a>")
