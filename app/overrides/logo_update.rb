cf46c0f6a20f172af3e8141e9760077c3df9aff1
m0zal <most_wanted@list.ru>
2017-09-16 11:06:56 +0200

    seven

--- app/overrides/logo_update.rb
+++ app/overrides/logo_update.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => 'spree/shared/_header',
 	:name => 'company logo replace',
 	:replace => 'figure#logo',
 	:text => '<a href="/">
-	<img src="/assets/images/giftucool_blue_logo.png" alt="Gifticool" class="col-md-2 col-sm-9"></a>'
+	<img src="/images/giftucool_blue_logo.png" alt="Gifticool" class="col-md-2 col-sm-9"></a>'
 	)
